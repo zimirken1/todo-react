2d45429a135e8c9adc3fe08b90eb35b2df3b1fea
zimirken1 <rumyan777777@gmail.com>
2023-07-12 10:22:19 +0300

    added some css styles and input validation

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,11 +1,38 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import {TodoContext} from '../context/index';
 import '../styles/TodoForm.css'
 import {FiPlus} from "react-icons/fi";
 
 const TodoForm = () => {
-    const [task, setTask] = useState('');
     const {addTodo} = useContext(TodoContext);
+    const [task, setTask] = useState('');
+    const [taskDirty, setTaskDirty] = useState(false);
+    const [taskError, setTaskError] = useState('Поле не может быть пустым');
+    const [formValid, setFormValid] = useState(false)
+
+    useEffect(() => {
+        if (taskError) {
+            setFormValid(false)
+        } else {
+            setFormValid(true)
+        }
+    }, [taskError])
+
+    const blurHandler = (e) => {
+        if (e.target.name === 'task') {
+            setTaskDirty(true)
+        }
+    }
+
+    const taskHandler = (e) => {
+        setTask(e.target.value);
+        const re = /^[a-zA-Z]{3,}$/;
+        if (!re.test(String(e.target.value).toLowerCase())) {
+            setTaskError('Некорректный ввод')
+        } else {
+            setTaskError("")
+        }
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -16,18 +43,24 @@ const TodoForm = () => {
     return (
         <div className={"form-wrapper"}>
             things to do
+            <div className={"form-valid"}>
+                {(taskDirty && taskError) && <div>{taskError}</div>}
+            </div>
             <form
                 onSubmit={handleSubmit}
                 className={"todo-form"}
             >
                 <input
+                    onBlur={(e) => blurHandler(e)}
+                    name={'task'}
                     className={"todo-form__input"}
                     type="text"
                     value={task}
-                    onChange={(e) => setTask(e.target.value)}
+                    onChange={(e) => taskHandler(e)}
                     placeholder="Введите задачу"
                 />
-                <button type="submit"><FiPlus size={30} className={"todo-form__add-btn"}/></button>
+                <button disabled={!formValid} type="submit"><FiPlus size={30} className={"todo-form__add-btn"}/>
+                </button>
             </form>
         </div>
     );
